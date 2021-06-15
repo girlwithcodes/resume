06fede64bdb79d5784f77f7df721564d04de8fba
girlwithcodes <heather.mantooth0@gmail.com>
2021-06-15 11:29:23 -0500

    Fixes error

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const fs = require("fs");
 
-fs.readFile(__dirname + "/info.json", "utf8", function(err, data {
+fs.readFile(__dirname + "/info.json", "utf8", function(err, data) {
   if (err) {
     console.error(error);
   } else {
     console.log(data);
     return data;
   }
-}));
+});
